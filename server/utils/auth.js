2584cfc744bd8ae9f496214ea9c32605cc94e1c5
lbako801 <lbako98@gmail.com>
2023-03-08 20:16:15 -0700

    updated backend. Need to implement frontend specs

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,53 +5,37 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
   authMiddleware: function ({ req, connection }, res, next) {
-    let token;
-    
-    if (req) {
-      // allows token to be sent via  req.query or headers
-      token = req.query.token || req.headers.authorization;
-  
-      // ["Bearer", "<tokenvalue>"]
-      if (req.headers.authorization) {
-        token = token.split(' ').pop().trim();
-      }
-    } else if (connection) {
-      // allows token to be sent via connectionParams
-      token = connection.context.Authorization;
-    }
+    const { token } = req.query || {};
+    const authHeader = req.headers.authorization || '';
+    const tokenParts = authHeader.split(' ');
+    const bearerToken = tokenParts.length === 2 && tokenParts[0] === 'Bearer' ? tokenParts[1] : null;
+    const finalToken = token || bearerToken;
 
-    if (!token) {
+    if (!finalToken) {
       return res.status(400).json({ message: 'You have no token!' });
     }
 
     try {
-      // verify token and get user data out of it
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      const { data } = require('jsonwebtoken').verify(finalToken, 'mysecretsshhhhh', { maxAge: '2h' });
       req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      if (req) {
+        req.context = {
+          user: req.user,
+        };
+      } else if (connection) {
+        connection.context = {
+          user: req.user,
+        };
+      }
+      next();
+    } catch (err) {
+      console.error('Error verifying token:', err);
+      return res.status(400).json({ message: 'Invalid token!' });
     }
-
-    // add the user data to the context
-    if (req) {
-      req.context = {
-        user: req.user,
-      };
-    } else if (connection) {
-      connection.context = {
-        user: req.user,
-      };
-    }
-
-    // send to next endpoint
-    next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    return require('jsonwebtoken').sign({ data: payload }, 'mysecretsshhhhh', { expiresIn: '2h' });
   },
 };

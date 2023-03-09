6f529253fe9748c035ff4b1a08ff1f9d8ea0e2a5
lbako801 <lbako98@gmail.com>
2023-03-08 20:37:19 -0700

    imported use hooks into signup and login

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,11 +1,12 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
+import { useMutation } from "@apollo/client";
+import { LOGIN } from "../utils/mutations";
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
+  const [loginUser] = useMutation(LOGIN);
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
